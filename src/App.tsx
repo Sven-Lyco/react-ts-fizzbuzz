72e7b52c00ac4295b137fa346a618bc28cd351f6
Sven Lüchow <sven.luechow@icloud.com>
2022-06-10 17:54:14 +0200

    Add two inputs and functions to show list of fizzbuzz

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import styled from 'styled-components';
 
 export default function App() {
   let [count, setCount] = useState<number>(1);
-
-  const handleIncrement = (): void => {
-    setCount((count += 1));
-  };
-
-  const handleDecrement = (): void => {
-    if (count > 1) {
-      setCount((count -= 1));
-    }
-  };
+  let [fizzBuzzArray, setFizzBuzzArray] = useState<string[]>([]);
+  let [firstInput, setFirstInput] = useState<number>(1);
+  let [secondInput, setSecondInput] = useState<number>(1);
 
   return (
     <Main>
@@ -33,8 +26,77 @@ export default function App() {
       </h2>
       <button onClick={handleDecrement}>-</button>
       <button onClick={handleIncrement}>+</button>
+      <InputWrapper>
+        <label htmlFor="first-number">First Number</label>
+        <input
+          type="number"
+          name="first-number"
+          id="first-number"
+          value={firstInput}
+          min={1}
+          step={1}
+          onChange={handleFirstInputChange}
+        />
+        <label htmlFor="second-number">Second Number</label>
+        <input
+          type="number"
+          name="second-number"
+          id="second-number"
+          value={secondInput}
+          min={1}
+          step={1}
+          onChange={handleSecondInputChange}
+        />
+        <button onClick={() => fizzBuzzFunction(firstInput, secondInput)}>
+          Show FizzBuzz
+        </button>
+      </InputWrapper>
+      <List role="list">
+        {fizzBuzzArray?.map((element, index) => {
+          return <li key={index}>{element}</li>;
+        })}
+      </List>
     </Main>
   );
+
+  function handleIncrement() {
+    setCount((count += 1));
+  }
+
+  function handleDecrement() {
+    if (count > 1) {
+      setCount((count -= 1));
+    }
+  }
+
+  function handleFirstInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setFirstInput(Number(event.target.value));
+  }
+
+  function handleSecondInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setSecondInput(Number(event.target.value));
+  }
+
+  function fizzBuzzFunction(firstInput: number, secondInput: number) {
+    let arr: string[] = [];
+
+    if (firstInput < secondInput) {
+      for (let i = firstInput; i <= secondInput; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+          arr.push('FizzBuzz');
+        } else if (i % 3 === 0) {
+          arr.push('Fizz');
+        } else if (i % 5 === 0) {
+          arr.push('Buzz');
+        } else {
+          arr.push(i.toString());
+        }
+      }
+      setFizzBuzzArray(arr);
+    } else {
+      alert('the second number must be greater than the first number ');
+    }
+  }
 }
 
 const Main = styled.main`
@@ -46,5 +108,22 @@ const Main = styled.main`
     font-weight: bolder;
     align-self: center;
     padding: 5px;
+  }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 25px;
+  margin: 50px 0;
+`;
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+
+  li {
+    padding-left: 8px;
   }
 `;
