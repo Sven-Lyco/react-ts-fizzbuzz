b2703ec3708e21894f05b04d8064547ff365cb0f
Sven Lüchow <sven.luechow@icloud.com>
2022-06-10 14:46:13 +0200

    Add FizzBuzz Function

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 export default function App() {
+  let [count, setCount] = useState<number>(1);
+
+  const handleIncrement = (): void => {
+    setCount((count += 1));
+  };
+
+  const handleDecrement = (): void => {
+    if (count > 1) {
+      setCount((count -= 1));
+    }
+  };
+
   return (
-    <Wrapper>
+    <Main>
       <h1>React Typescript Fizz Buzz</h1>
       <p>
         Counting incrementally, replacing any number divisible by three with the
-        word "fizz", and any number divisible by five with the word "buzz". For
+        word "Fizz", and any number divisible by five with the word "Buzz". For
         numbers which are multiples of both three and five print “FizzBuzz”
       </p>
-      <h2>0</h2>
-      <button>-</button>
-      <button>+</button>
-    </Wrapper>
+      <h2>
+        {count % 15 === 0
+          ? 'FizzBuzz'
+          : count % 3 === 0
+          ? 'Fizz'
+          : count % 5 === 0
+          ? 'Buzz'
+          : count}
+      </h2>
+      <button onClick={handleDecrement}>-</button>
+      <button onClick={handleIncrement}>+</button>
+    </Main>
   );
 }
 
-const Wrapper = styled.div`
+const Main = styled.main`
+  padding: 10px;
   text-align: center;
 
   button {
     font-size: larger;
+    font-weight: bold;
     align-self: center;
+    padding: 5px;
   }
 `;
