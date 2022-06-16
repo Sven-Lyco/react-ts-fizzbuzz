27446084bfd1c9aa9318d82755b96556dcb9a88d
Sven Lüchow <sven.luechow@icloud.com>
2022-06-16 08:59:45 +0200

    Update test for button

--- src/components/Button.spec.tsx
+++ src/components/Button.spec.tsx
@@ -3,20 +3,20 @@ import userEvent from '@testing-library/user-event';
 import Button from './Button';
 
 const onClick = jest.fn();
-const children = 'Hello';
+const children = 'Show FizzBuzz';
 
 describe('Button', () => {
   it('renders a button', () => {
     render(<Button onClick={onClick}>{children}</Button>);
 
-    const button = screen.getByRole('button', { name: /hello/i });
+    const button = screen.getByRole('button', { name: 'Show FizzBuzz' });
     expect(button).toBeInTheDocument();
   });
 
   it('has onClick function', () => {
     render(<Button onClick={onClick}>{children}</Button>);
 
-    const button = screen.getByRole('button', { name: /hello/i });
+    const button = screen.getByRole('button', { name: 'Show FizzBuzz' });
     userEvent.click(button);
     expect(onClick).toHaveBeenCalled();
   });
