29a9423601e3e8363a49ae45e548ede12fe61f2d
Sven Lüchow <sven.luechow@icloud.com>
2022-06-24 18:32:36 +0200

    Write Test for useCount

---
+++ src/hooks/useCount.spec.ts
@@ -0,0 +1,27 @@
+import { renderHook, act } from '@testing-library/react';
+import useCount from './useCount';
+
+test('should use counter', () => {
+  const { result } = renderHook(() => useCount());
+
+  expect(result.current.count).toBe(1);
+  expect(typeof result.current.handleIncrementCount).toBe('function');
+});
+
+test('should increment counter', () => {
+  const { result } = renderHook(() => useCount());
+
+  act(() => {
+    result.current.handleIncrementCount();
+  });
+  expect(result.current.count).toBe(2);
+});
+
+test('should decrement counter', () => {
+  const { result } = renderHook(() => useCount());
+
+  act(() => {
+    result.current.handleDecrementCount();
+  });
+  expect(result.current.count).toBe(1);
+});
